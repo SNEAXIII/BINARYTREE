1f07c33ef6ab30fca13f29a14ef3ebaf3109ae11
Mister Balise <74428850+SNEAXIII@users.noreply.github.com>
2021-10-22 09:51:09 +0000

    ca avance

--- main.py
+++ main.py
@@ -19,11 +19,7 @@ se programme :
 """
 arb = Noeud(Noeud(None,'B',Noeud(None,'C',None)),'A',Noeud(None,'D',None))
 
-##Exercice 3
-
 arb1 = Noeud(Noeud(None,4,None),5,Noeud(None,3,None))
-
-arb2 = Noeud(Noeud(Noeud(None,3,Noeud(None,4,None))),1,None(None,2,Noeud(None,5,None))
 
 ##Scripts cours 3.2
 
@@ -31,36 +27,43 @@ def taille(arb):
     """nombre de noeuds de arb"""
     if arb is None:
         return 0
-    return 1 + taille(arb)
+    return 1 + taille(arb.gauche) + taille(arb.droit)
+
+print(taille(arb))
 
 def hauteur(arb):
-    """renvoie la hauteur de l'arbre"""
+    #renvoie la hauteur de l'arbre
     if arb is None:
-        return 
-    return 1 + 
+        return 0
+    return 1 + max(hauteur(arb.gauche),hauteur(arb.droit))
     
+print(hauteur(arb))
+
 ##Exercice 6 parcours Prefixe et Postfixe
+
 def parcoursInfixe(arb):
-    """affiche les éléments de a lors d'un parcours infixe"""
+    #affiche les éléments de a lors d'un parcours infixe
     if arb is None:
         return
     parcoursInfixe(arb.gauche)
     print(arb.valeur,end=",")
     parcoursInfixe(arb.droit)
 
-"""on vérifie le script effectué manuellement"""
-def parcoursPrefixe(arb):
+#on vérifie le script effectué manuellement
+def parcoursPrefixe(arb):pass
+  
     
-def parcoursPostfixe(arb):
-
+def parcoursPostfixe(arb):pass
+""" 
 ##Exercice 7
 
-def affiche(arb):
+def affiche(arb):pass
     
 ##Exercice 9
 
-def parfait(h):
+def parfait(h):pass
 
-def peigneGauche(h):
+def peigneGauche(h):pass
 
-def peigneDroit(h):
+def peigneDroit(h):pass
+"""
