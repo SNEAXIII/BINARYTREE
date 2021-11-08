0073e8afacd52d2970ceeb50f7c565e491498c49
Mister Balise <74428850+SNEAXIII@users.noreply.github.com>
2021-11-08 07:45:09 +0000

    rien

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 class Noeud:
     """noeud d'un arbre binaire"""
+
     def __init__(self,g,v,d):
         self.gauche = g
         self.valeur = v
         self.droit = d
 
 #Exercice 8 : def __eq__
+
+    def _eq_(self,other):
+      if self.gauche == other.gauche and self.valeur == other.valeur and self.droit == other.droit :
+        return True
+      return False
 
 """
       "A"
@@ -44,26 +50,49 @@ print(hauteur(arb))
 def parcoursInfixe(arb):
     #affiche les éléments de a lors d'un parcours infixe
     if arb is None:
-        return
+        return None
     parcoursInfixe(arb.gauche)
     print(arb.valeur,end=",")
     parcoursInfixe(arb.droit)
 
 #on vérifie le script effectué manuellement
-def parcoursPrefixe(arb):pass
-  
+def parcoursPrefixe(arb):
+  if arb is None :
+    return None
+  print(arb.valeur,end=",")
+  parcoursPrefixe(arb.gauche)
+  parcoursPrefixe(arb.droit)
     
-def parcoursPostfixe(arb):pass
-""" 
+def parcoursPostfixe(arb):
+  if arb is None :
+    return None
+  parcoursPostfixe(arb.gauche)
+  parcoursPostfixe(arb.droit)
+  print(arb.valeur,end=",")
+
+arb_exo_7 = Noeud(Noeud(Noeud(None,3,Noeud(None,4,None)),1,None),2,Noeud(None,5,None))
+
 ##Exercice 7
 
-def affiche(arb):pass
-    
+def affiche(arb):
+  if arb is None:
+    return ''
+  print('(',end='')
+  affiche(arb.gauche)
+  print(str(arb.valeur),end='')
+  affiche(arb.droit)
+  print(')',end='')
+  
+print(affiche(arb_exo_7))
+print(arb_exo_7 == arb_exo_7)
+
 ##Exercice 9
 
-def parfait(h):pass
+def parfait(h):
+  assert type(h) == int and h>=0, "La hauteur est un entier positif"
+  if 
+  Noeud(parfait(h-1))
 
 def peigneGauche(h):pass
 
 def peigneDroit(h):pass
-"""
